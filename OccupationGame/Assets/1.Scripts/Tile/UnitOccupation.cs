70882be5dc7ec4649334c7759dc304467f3974a7
yuseungwon0318 <sw060318@naver.com>
2022-12-27 00:01:48 +0900

    Player & Enemy TileOccupation

--- OccupationGame/Assets/1.Scripts/Tile/UnitOccupation.cs
+++ OccupationGame/Assets/1.Scripts/Tile/UnitOccupation.cs
@@ -5,53 +5,117 @@ using UnityEngine;
 public class UnitOccupation : MonoBehaviour
 {
     public bool FirstAccess;
+    public bool Acess;
+
+    GameObject Unit;
+
     private SpriteRenderer Tile;
+
+    int OccupationLevel;
+
+    float TileAlpha;
+    float TileColor1;
+    float TileColor2;
+    float TileColor3;
+
    
-
-    private EdgeCollider2D Edge;
-    private PolygonCollider2D Polygon;
-
-
-    private float TileGap;
-    private LayerMask LayerMask;
-
+   
+    
     private void Start()
     {
-        Edge = GetComponent<EdgeCollider2D>();
-        Polygon = GetComponent<PolygonCollider2D>();
+        Tile = GetComponent<SpriteRenderer>();
+        TileColor1 = 0f;
+        TileColor2 = 0f;
+        TileColor3 = 0f;
 
-        
+        TileAlpha = 1f;
+        Acess = false;
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (FirstAccess)
-        {
-
-            //GameObject Prefabtile = GameObject.Instantiate(TilePrefab);
-            //Prefabtile.transform.position = this.transform.position;
-
-
-
-            Tile = this.GetComponent<SpriteRenderer>();
-            Tile.GetComponent<SpriteRenderer>().color = new Color(255f, 0f, 0f, 0.25f);
-
-
-
-            FirstAccess = false;
-        }
+        Unit = collision.gameObject;
+        Acess = true;
     }
-        
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Acess = false;
+    }
 
-        
-        
-    
-  
+
     public void TurnEnd()
     {
         Debug.Log("ео а╬╥А");
-        float TileA = gameObject.GetComponent<SpriteRenderer>().color.a;
-        TileA += 0.25f;
+
+        if(Acess)
+        {
+            if (Unit.tag == "Player")
+            {
+                OccupationLevel += 1;
+            }
+            else if (Unit.tag == "Enemy")
+            {
+                OccupationLevel -= 1;
+            }
+        }
+
+        switch (OccupationLevel)
+        {
+            case -4:
+                Blue();
+                TileAlpha = 1f;
+                break;
+            case -3:
+                Blue(); 
+                TileAlpha = 0.75f;
+                break;
+            case -2:
+                Blue();
+                TileAlpha = 0.5f;
+                break;
+            case -1:
+                Blue();
+                TileAlpha = 0.25f;
+                break;
+            case 0:
+                TileColor1 = 0;
+                TileColor2 = 0;
+                TileColor3 = 0;
+                TileAlpha = 0;
+                break;
+            case 1:
+                Red();
+                TileAlpha = 0.25f;
+                break;
+            case 2:
+                Red();
+                TileAlpha = 0.5f;
+                break;
+            case 3:
+                Red();
+                TileAlpha = 0.75f;
+                break;
+            case 4:
+                Red();
+                TileAlpha = 1f;
+                break;
+        }
+
+        Tile.color = new Color(TileColor1, TileColor2, TileColor3, TileAlpha);
+        
+    }
+
+    public void Red()
+    {
+        TileColor1 = 255f;
+        TileColor2 = 0f;
+        TileColor3 = 0f;
+    }
+    public void Blue()
+    {
+        TileColor1 = 0f;
+        TileColor2 = 0f;
+        TileColor3 = 255f;
     }
 
    
@@ -62,12 +126,7 @@ public class UnitOccupation : MonoBehaviour
 
     }
 
-    public void OccupationAround()
-    {
-        Tile.GetComponent<SpriteRenderer>().color = new Color(255f, 0f, 0f, 1f);
-
-        this.tag = "Occupation";
-    }
+    
 
 
 
